c70e90709bb4db1e2e5600ba8b96a42e4688179b
harrison <haohu.shen@ucalgary.ca>
2019-07-08 13:35:30 -0600

    updated

---
+++ PAT_Advanced_Level_Practice/1133.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <cstdio>
+#include <unordered_set>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAXN = 20;
+char s[MAXN], s2[MAXN];
+
+struct Node
+{
+    int val = -1;
+    string currentAddress;
+    string nextAddress;
+};
+
+int main()
+{
+    int n, k;
+    string currentAddress;
+
+    scanf("%s %d %d", s, &n, &k);
+    getchar();
+
+    if (n == 0) {
+        return 0;
+    }
+
+    currentAddress = s;
+    int i;
+    unordered_map <string, Node> save;
+    Node tmp;
+
+    for (i = 0; i < n; ++i) {
+        scanf("%s %d %s", s, &tmp.val, s2);
+        tmp.currentAddress = s;
+        tmp.nextAddress = s2;
+        save[tmp.currentAddress] = tmp;
+    }
+
+    vector <Node> A;
+    vector <Node> B;
+    vector <Node> C;
+
+    while (true) {
+        if (currentAddress == "-1") {
+            break;
+        }
+        if ( save[currentAddress].val < 0 ) {
+            A.push_back(save[currentAddress]);
+        } else if ( save[currentAddress].val <= k ) {
+            B.push_back(save[currentAddress]);
+        } else {
+            C.push_back(save[currentAddress]);
+        }
+        currentAddress = save[currentAddress].nextAddress;
+    }
+
+
+    copy(B.begin(), B.end(), back_inserter(A));
+    copy(C.begin(), C.end(), back_inserter(A));
+
+    int cntA = static_cast<int>(A.size());
+    if (cntA == 0) {
+        printf("0 %s -1\n", s);
+        return 0;
+    }
+    for (i = 0; i < cntA - 1; ++i) {
+        A[i].nextAddress = A[i + 1].currentAddress;
+    }
+    A[cntA - 1].nextAddress = "-1";
+    for (i = 0; i < cntA; ++i) {
+        printf("%s %d %s\n", A[i].currentAddress.c_str(), A[i].val, A[i].nextAddress.c_str());
+    }
+
+    return 0;
+}
