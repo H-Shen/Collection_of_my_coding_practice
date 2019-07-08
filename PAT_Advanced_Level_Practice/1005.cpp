3d1192256e6a95e749a3b380bc4f713e62f2164c
harrison <haohu.shen@ucalgary.ca>
2019-07-08 06:54:46 -0600

    updated

---
+++ PAT_Advanced_Level_Practice/1005.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const vector<string> dict{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int main() {
+    string s;
+    cin >> s;
+    int sum = 0;
+    for (const auto &p : s) { sum = sum + (p - '0'); }
+    string num = to_string(sum);
+    string res;
+
+    for (int i = 0; i < (int) num.size() - 1; ++i) {
+        res += dict[num[i] - '0'] + ' ';
+    }
+    res += dict[num[(int) num.size() - 1] - '0'];
+    cout << res << endl;
+
+    return 0;
+}
---
+++ PAT_Advanced_Level_Practice/1009.cpp
@@ -0,0 +1,102 @@
+#include <cstdio>
+
+#include <string>
+#include <unordered_set>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+const double eps = 1e-6;
+
+inline int sgn(double a)
+{
+    if (a < -eps) return -1;
+    if (a < eps)  return 0;
+    return 1;
+}
+
+struct item
+{
+    int exp;
+    double coeff;
+};
+
+struct item operator * (const item &a, const item &b)
+{
+    item res;
+    res.exp = a.exp + b.exp;
+    res.coeff = a.coeff * b.coeff;
+    return res;
+}
+
+bool cmp (const item &a, const item &b)
+{
+    return (a.exp > b.exp);
+}
+
+vector <item> mulArr(const vector <item> &A, const vector <item> &B)
+{
+    vector <item> res;
+
+    if (A.empty() || B.empty()) {
+        return res;
+    }
+
+    int lenA = A.size();
+    int lenB = B.size();
+
+    int i, j;
+    item tmp;
+    unordered_map <int, item> save;
+
+    for (i = 0; i < lenA; ++i) {
+        for (j = 0; j < lenB; ++j) {
+            tmp = A[i] * B[j];
+            save[tmp.exp].coeff += tmp.coeff;
+            save[tmp.exp].exp = tmp.exp;
+        }
+    }
+    
+    for (auto it = save.cbegin(); it != save.cend(); ++it) {
+        if ( sgn(it->second.coeff) != 0 ) {
+            res.push_back(it->second);
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    int N, i;
+    item tmp;
+
+    scanf("%d", &N);
+    vector <item> A(N);
+    for (i = 0; i < N; ++i) {
+        scanf("%d %lf", &tmp.exp, &tmp.coeff);
+        A[i] = tmp;
+    }
+
+    scanf("%d", &N);
+    vector <item> B(N);
+    for (i = 0; i < N; ++i) {
+        scanf("%d %lf", &tmp.exp, &tmp.coeff);
+        B[i] = tmp;
+    }
+
+    vector <item> C = mulArr(A, B);
+
+    if (C.empty()) {
+        printf("0");
+    } else {
+        sort(C.begin(), C.end(), cmp);
+        printf("%d", (int)C.size());
+        for (i = 0; i < (int)C.size(); ++i) {
+            printf(" %d %.1lf", C[i].exp, C[i].coeff);
+        }
+    }
+    return 0;
+}
