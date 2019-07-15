f8dd46d93ae5c12fbb9ce9c3335d2f71f9f17a6d
harrison <haohu.shen@ucalgary.ca>
2019-07-15 01:17:34 -0600

    updated

--- Hackerrank/C/Small_Triangles_Large_Triangles.c
+++ Hackerrank/C/Small_Triangles_Large_Triangles.c
@@ -41,3 +41,16 @@ void sort_by_area(triangle* tr, int n) {
 }
 
 int main()
+{
+	int n;
+	scanf("%d", &n);
+	triangle *tr = malloc(n * sizeof(triangle));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
+	sort_by_area(tr, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+	}
+	return 0;
+}
