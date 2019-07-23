b3c57d28fb756f08134b988a3bbd1ad8babfa526
harrison <haohu.shen@ucalgary.ca>
2019-07-22 22:43:39 -0600

    updated

---
+++ Hackerrank/Data_Structures/Delete_a_Node.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+
+    int length = 0;
+    auto currentNode = head;
+    while (currentNode != nullptr) {
+        ++length;
+        currentNode = currentNode->next;
+    }
+
+    if (position < 0 || position >= length) {
+        throw invalid_argument("");
+    }
+
+    // HEAD
+    if (position == 0) {
+        currentNode = head;
+        head = currentNode->next;
+        return head;
+    }
+
+    // TAIL
+    if (position == length - 1) {
+        currentNode = head;
+        while (currentNode->next->next != nullptr) {
+            currentNode = currentNode->next;
+        }
+        currentNode->next = nullptr;
+        return head;
+    }
+
+    // DELETE OTHER NODES
+    currentNode = head;
+    for (int i = 0; i < position - 1; ++i) {
+        currentNode = currentNode->next;
+    }
+    auto temp = currentNode->next->next;
+    currentNode->next = temp;
+    return head;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist1 = deleteNode(llist->head, position);
+
+    print_singly_linked_list(llist1, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist1);
+
+    fout.close();
+
+    return 0;
+}
