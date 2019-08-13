89f817edb4c118601a8192352f21682b0e880d2e
harrison <haohu.shen@ucalgary.ca>
2019-08-13 07:14:27 -0600

    updated

---
+++ Miscellaneous/calcNthFibWithCppTemplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<int N>
+struct Fib {
+    enum : unsigned long long {
+        value = Fib<N - 1>::value + Fib<N - 2>::value
+    };
+};
+
+template<>
+struct Fib<1> {
+    enum : unsigned long long {
+        value = 1
+    };
+};
+
+template<>
+struct Fib<2> {
+    enum : unsigned long long {
+        value = 1
+    };
+};
+
+int main() {
+    static_assert(Fib<1>::value == 1, "");
+    static_assert(Fib<2>::value == 1, "");
+    static_assert(Fib<93>::value == 12200160415121876738UL, "");
+    return 0;
+}
