0f4ecd612325f8078deb30cd76ef1aabf2be2f6b
harrison <haohu.shen@ucalgary.ca>
2019-08-17 11:09:22 -0600

    updated

--- Miscellaneous/fastIO.cpp
+++ Miscellaneous/fastIO.cpp
@@ -1,10 +1,11 @@
 /**
- * An implementation of optimization on reading (读入优化) an integer from stdin.
+ * An implementation of optimization on reading (读入优化) an integer from stdin and writing (输出优化) an integer to stdout.
  * It has been tested in Linux csx.cs.ucalgary.ca (g++ -std=c++14 -Wall -O2),
  * but the strange thing is it is slower than scanf() in my macOS, (homebrew g++-7 -std=c++14 -Wall -O2)
  * So make sure to run the tests before using it.
  */
 #include <bits/stdc++.h>
+#include <unistd.h>
 
 template<typename T>
 inline static
@@ -15,27 +16,45 @@ void readInt(T &x) {
     char ch = static_cast<char>(getchar());                // Notice ch < '0' since '0' == 48
 
     // skip all non digit characters
-    while (!isdigit(ch)) {      // go into the loop if ch is not a digit
+    while (!isdigit(ch)) {                                  // go into the loop if ch is not a digit
         if (ch == '-') {
-            isNeg = true;               // change isNeg if the number is negative
+            isNeg = true;                                   // change isNeg if the number is negative
         }
         ch = static_cast<char>(getchar());                 // continue to get a character
     }
 
-    while (isdigit(ch)) {    // go into the loop if ch is a digit
-        x = x * 10 + static_cast<T>(ch ^ 48);        // append the new value of ch to the end of x
-        ch = static_cast<char>(getchar());                 // continue to get a character
+    while (isdigit(ch)) {                       // go into the loop if ch is a digit
+        x = x * 10 + static_cast<T>(ch ^ 48);               // append the new value of ch to the end of x
+        ch = static_cast<char>(getchar());                      // continue to get a character
     }
     x = isNeg ? -x : x;
 }
 
-void testInt(const size_t &length) {
+template<typename T>
+inline static
+void writeInt(T x) {
+    if (x < 0) {
+        x = -x;
+        putchar('-');
+    }
+    static int storeDigits[40];
+    int top = 0;
+    do {
+        storeDigits[top++] = x % 10;
+        x /= 10;
+    } while (x);
+    while (top) {
+        putchar(storeDigits[--top] + 48);
+    }
+}
+
+void testReadInt(const size_t &length) {
 
     auto arr0 = std::make_unique<int[]>(length);
     auto arr1 = std::make_unique<int[]>(length);
     const char *fileName("testData.in");
 
-    /* createTestData */
+    /* create TestData */
 
     // Initialize a random number generator.
     std::random_device dev;
@@ -62,7 +81,7 @@ void testInt(const size_t &length) {
 
     std::chrono::time_point<std::chrono::steady_clock> start, stop;
     std::chrono::duration<double> elapsed_in_seconds{};
-    std::cout << "Test on " << length << " ints..." << std::endl;
+    std::cout << "Test on reading " << length << " ints..." << std::endl;
 
     // Benchmark on scanf()
     freopen(fileName, "r", stdin);
@@ -72,7 +91,7 @@ void testInt(const size_t &length) {
     }
     stop = std::chrono::steady_clock::now();
     elapsed_in_seconds = stop - start;
-    std::cout << "Scanf: " << elapsed_in_seconds.count() << " seconds" << std::endl;
+    std::cout << "scanf: " << elapsed_in_seconds.count() << " seconds" << std::endl;
     fclose(stdin);
 
     // Benchmark on readInt()
@@ -83,7 +102,7 @@ void testInt(const size_t &length) {
     }
     stop = std::chrono::steady_clock::now();
     elapsed_in_seconds = stop - start;
-    std::cout << "ReadInt: " << elapsed_in_seconds.count() << " seconds" << std::endl;
+    std::cout << "readInt: " << elapsed_in_seconds.count() << " seconds" << std::endl;
     fclose(stdin);
 
     // Compare arr0 and arr1 just in case
@@ -96,13 +115,13 @@ void testInt(const size_t &length) {
 
 }
 
-void testLongLong(const size_t &length) {
+void testReadLongLong(const size_t &length) {
 
     auto arr0 = std::make_unique<long long[]>(length);
     auto arr1 = std::make_unique<long long[]>(length);
     const char *fileName("testData.in");
 
-    /* createTestData */
+    /* create TestData */
 
     // Initialize a random number generator.
     std::random_device dev;
@@ -129,7 +148,7 @@ void testLongLong(const size_t &length) {
 
     std::chrono::time_point<std::chrono::steady_clock> start, stop;
     std::chrono::duration<double> elapsed_in_seconds{};
-    std::cout << "Test on " << length << " long long ints..." << std::endl;
+    std::cout << "Test on reading " << length << " long long ints..." << std::endl;
 
     // Benchmark on scanf()
     freopen(fileName, "r", stdin);
@@ -139,7 +158,7 @@ void testLongLong(const size_t &length) {
     }
     stop = std::chrono::steady_clock::now();
     elapsed_in_seconds = stop - start;
-    std::cout << "Scanf: " << elapsed_in_seconds.count() << " seconds" << std::endl;
+    std::cout << "scanf: " << elapsed_in_seconds.count() << " seconds" << std::endl;
     fclose(stdin);
 
     // Benchmark on readInt()
@@ -150,7 +169,7 @@ void testLongLong(const size_t &length) {
     }
     stop = std::chrono::steady_clock::now();
     elapsed_in_seconds = stop - start;
-    std::cout << "ReadInt: " << elapsed_in_seconds.count() << " seconds" << std::endl;
+    std::cout << "readInt: " << elapsed_in_seconds.count() << " seconds" << std::endl;
     fclose(stdin);
 
     // Compare arr0 and arr1 just in case
@@ -163,11 +182,62 @@ void testLongLong(const size_t &length) {
 
 }
 
+void testWriteInt(const size_t &length) {
+
+    const char *fileName0("testData0.out");
+    const char *fileName1("testData1.out");
+
+    // Initialize a random number generator.
+    std::random_device dev;
+    std::mt19937 random_generator(dev());
+    std::uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
+
+    // Initialize an array with given size of random numbers.
+    auto arr = std::make_unique<int[]>(length);
+    for (size_t i = 0; i != length; ++i) {
+        arr[i] = dist(random_generator);
+    }
+
+    /* Define some variables for the test */
+    std::chrono::time_point<std::chrono::steady_clock> start, stop;
+    std::chrono::duration<double> elapsed_in_seconds0{};
+    std::chrono::duration<double> elapsed_in_seconds1{};
+    std::cout << "Test on writing " << length << " ints..." << std::endl;
+
+    // Benchmark on printf()
+    freopen(fileName0, "w", stdout);
+    start = std::chrono::steady_clock::now();
+    for (size_t i = 0; i != length; ++i) {
+        printf("%d\n", arr[i]);
+    }
+    stop = std::chrono::steady_clock::now();
+    elapsed_in_seconds0 = stop - start;
+
+    // Benchmark on writeInt()
+    freopen(fileName1, "w", stdout);
+    start = std::chrono::steady_clock::now();
+    for (size_t i = 0; i != length; ++i) {
+        writeInt<int>(arr[i]);
+        putchar('\n');
+    }
+    stop = std::chrono::steady_clock::now();
+    elapsed_in_seconds1 = stop - start;
+
+    fclose(stdout);
+    std::cerr << "printf  : " << elapsed_in_seconds0.count() << " seconds" << std::endl;
+    std::cerr << "writeInt: " << elapsed_in_seconds1.count() << " seconds" << std::endl;
+
+    /* remove TestData */
+    std::remove(fileName0);
+    std::remove(fileName1);
+}
+
 int main() {
 
     constexpr size_t length = 5000000;
-    testInt(length);
-    testLongLong(length);
+    testReadInt(length);
+    testReadLongLong(length);
+    testWriteInt(length);
 
     return 0;
 }
