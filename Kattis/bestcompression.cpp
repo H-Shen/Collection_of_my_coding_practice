9aa2cac591868e10e54963361ee4a3e369641b4f
harrison <haohu.shen@ucalgary.ca>
2019-08-27 16:46:25 -0600

    updated

---
+++ Kattis/bestcompression.cpp
@@ -0,0 +1,32 @@
+// https://open.kattis.com/problems/bestcompression
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+inline static
+ll powerOfTwo(const int &n) {
+    ll result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= 2;
+    }
+    return result;
+}
+
+int main() {
+
+    ll N;
+    int b;
+    cin >> N >> b;
+    ll allPossibleSituations = powerOfTwo(b + 1) - 1;
+    if (N <= allPossibleSituations) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+    cout << endl;
+
+
+    return 0;
+}
