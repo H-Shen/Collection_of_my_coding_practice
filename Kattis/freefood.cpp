3d1c167c280ce7e1be0bd0f2f0e17fd1955854c4
harrison <haohu.shen@ucalgary.ca>
2019-09-11 01:00:08 -0600

    updated

---
+++ Kattis/freefood.cpp
@@ -0,0 +1,22 @@
+// https://open.kattis.com/problems/freefood
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int MAXN = 370;
+int s[MAXN];
+
+int main() {
+
+    int n, left, right;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%d %d", &left, &right);
+        for (int i = left; i <= right; ++i) {
+            s[i] = 1;
+        }
+    }
+    printf("%lu\n", count(s, s + MAXN, 1));
+
+    return 0;
+}
