ef0ad6a3da3d6e5338b3ae332058a7a7c5da62d4
harrison <haohu.shen@ucalgary.ca>
2019-09-21 19:24:04 -0600

    updated

---
+++ Kattis/halfacookie.cpp
@@ -0,0 +1,37 @@
+// https://open.kattis.com/problems/halfacookie
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline static
+int sgn(const double &x) {
+    double eps = 1e-8;
+    if (x > eps) {
+        return 1;
+    }
+    if (x > -eps) {
+        return 0;
+    }
+    return -1;
+}
+
+int main() {
+
+    double r, x, y, s, theta, circular_sector, triangle_square, a_big, a_small;
+    while (~scanf("%lf %lf %lf", &r, &x, &y)) {
+        s = x * x + y * y;
+        if (sgn(s - r * r) > 0) {
+            printf("miss\n");
+        } else {
+            theta = acos((4 * s - 2 * r * r) / (2 * r * r));
+            circular_sector = theta * r * r / 2;
+            triangle_square = sqrt(r * r - s) * sqrt(s);
+            a_small = circular_sector - triangle_square;
+            a_big = M_PI * r * r - a_small;
+            printf("%.6lf %.6lf\n", a_big, a_small);
+        }
+    }
+
+    return 0;
+}
