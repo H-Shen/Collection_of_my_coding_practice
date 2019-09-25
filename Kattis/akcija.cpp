eb5ac68a5c445a08b0ffeb3a01b95ac1352df679
harrison <haohu.shen@ucalgary.ca>
2019-09-24 18:37:42 -0600

    updated

---
+++ Kattis/akcija.cpp
@@ -0,0 +1,32 @@
+// https://open.kattis.com/problems/akcija
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    vector<ll> A(n);
+    ll sum = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%lld", &A.at(i));
+        sum += A.at(i);
+    }
+    sort(A.begin(), A.end(), [](const ll &lhs, const ll &rhs) {
+        return (lhs > rhs);
+    });
+    int counter = 0;
+    for (int i = 0; i < n; ++i) {
+        ++counter;
+        if (counter == 3) {
+            sum -= A.at(i);
+            counter = 0;
+        }
+    }
+    printf("%lld\n", sum);
+
+    return 0;
+}
---
+++ Kattis/chopin.cpp
@@ -0,0 +1,40 @@
+// https://open.kattis.com/problems/chopin
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<string, string> alternative;
+
+int main() {
+
+    alternative["A#"] = "Bb";
+    alternative["Bb"] = "A#";
+    alternative["C#"] = "Db";
+    alternative["Db"] = "C#";
+    alternative["D#"] = "Eb";
+    alternative["Eb"] = "D#";
+    alternative["F#"] = "Gb";
+    alternative["Gb"] = "F#";
+    alternative["G#"] = "Ab";
+    alternative["Ab"] = "G#";
+
+    int n = 1;
+    string note, tonality, alternativeNote, alternativeComb;
+    while (cin >> note >> tonality) {
+        alternativeNote = "";
+        if (alternative.find(note) != alternative.end()) {
+            alternativeNote = alternative[note];
+        }
+        alternativeComb = alternativeNote + " " + tonality;
+
+        if (!alternativeNote.empty()) {
+            cout << "Case " << n << ": " << alternativeComb << endl;
+        } else {
+            cout << "Case " << n << ": UNIQUE" << endl;
+        }
+        ++n;
+    }
+
+    return 0;
+}
