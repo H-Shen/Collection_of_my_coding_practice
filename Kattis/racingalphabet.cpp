4d2eff4b49bd39be2ca4eddfa6d69c9a3b1dc4d7
harrison <haohu.shen@ucalgary.ca>
2019-10-03 10:04:02 -0600

    updated

---
+++ Kattis/racingalphabet.cpp
@@ -0,0 +1,45 @@
+// https://open.kattis.com/problems/racingalphabet
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<char, int> A;
+
+int minimalDistance(char a, char b) {
+    int d1 = abs(A[a] - A[b]);
+    int d2 = 28 - abs(A[a] - A[b]);
+    return min(d1, d2);
+}
+
+int main() {
+
+    for (char ch = 'A'; ch <= 'Z'; ++ch) {
+        A[ch] = (ch - 'A') + 2;
+    }
+    A[' '] = 0;
+    A['\''] = 1;
+
+    int n;
+    cin >> n;
+    cin.get();
+    while (n--) {
+
+        string s;
+        getline(cin, s);
+        int distance = 0;
+        int time = 0;
+        int length = static_cast<int>(s.size());
+        for (int i = 0; i < length - 1; ++i) {
+            distance += minimalDistance(s[i], s[i + 1]);
+            ++time;
+        }
+        ++time;
+        double D = M_PI * 60.0 / 28 * distance;
+        double T = time * 1.0 + D / 15.0;
+        printf("%.10lf\n", T);
+    }
+
+
+    return 0;
+}
