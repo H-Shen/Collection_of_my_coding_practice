b148b7c9c05e17da9d57ce67e5e13f9b256bb699
harrison <haohu.shen@ucalgary.ca>
2019-10-07 14:31:19 -0600

    updated

---
+++ Kattis/empleh.cpp
@@ -0,0 +1,111 @@
+// https://open.kattis.com/problems/empleh
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int MAXN = 8;
+
+vector<vector<char> > chessboard(MAXN, vector<char>(MAXN));
+
+inline static
+pair<int, int> convert(char x, int y) {
+    pair<int, int> result;
+    result.first = (int) (x - 'a');
+    result.second = MAXN - y;
+    return result;
+}
+
+inline static
+std::vector<std::string> splitByChar(std::string s, const char &delim) {
+    std::vector<std::string> res;
+    std::istringstream f(s);
+    std::string temp;
+    while (getline(f, s, delim)) {
+        res.emplace_back(s);
+    }
+    return res;
+}
+
+inline static
+void parse(const string &s, bool isWhite) {
+    auto split0 = splitByChar(s, ' ');
+    if (split0.size() > 1) {
+        auto split1 = splitByChar(split0.at(1), ',');
+        char chess;
+        int row;
+        int col;
+        for (const auto &i : split1) {
+            // chess
+            if (i.size() == 2) {
+                if (isWhite) {
+                    chess = 'P';
+                } else {
+                    chess = 'p';
+                }
+            } else {
+                if (isWhite) {
+                    chess = (char) toupper(i.at(0));
+                } else {
+                    chess = (char) tolower(i.at(0));
+                }
+            }
+            if (i.size() == 2) {
+                auto p = convert(i.at(0), i.at(1) - '0');
+                row = p.first;
+                col = p.second;
+            } else {
+                auto p = convert(i.at(1), i.at(2) - '0');
+                row = p.first;
+                col = p.second;
+            }
+            chessboard.at(col).at(row) = chess;
+        }
+    }
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    string s;
+    getline(cin, s);
+    parse(s, true);
+    getline(cin, s);
+    parse(s, false);
+    bool change;
+
+    for (int i = 0, index = 0; i < 2 * MAXN + 1; ++i) {
+        if (i % 2 == 0) {
+            cout << "+---+---+---+---+---+---+---+---+" << endl;
+        } else {
+            if (index % 2 == 0) {
+                change = true;
+            } else {
+                change = false;
+            }
+            string content;
+            for (int j = 0; j < MAXN; ++j) {
+                cout << "|";
+                if (isalpha(chessboard.at(index).at(j))) {
+                    content = chessboard.at(index).at(j);
+                } else {
+                    if (change) {
+                        content = ".";
+                    } else {
+                        content = ":";
+                    }
+                }
+                if (change) {
+                    content = "." + content + ".";
+                } else {
+                    content = ":" + content + ":";
+                }
+                change = !change;
+                cout << content;
+            }
+            cout << "|" << endl;
+            ++index;
+        }
+    }
+
+    return 0;
+}
---
+++ Kattis/helpme.cpp
@@ -0,0 +1,134 @@
+// https://open.kattis.com/problems/helpme
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<char, int> orderOfType = {
+        {'k', 5},
+        {'q', 4},
+        {'r', 3},
+        {'b', 2},
+        {'n', 1},
+        {'p', 0}
+};
+
+struct Chess {
+    char chess;
+    char x;
+    int y;
+
+    explicit Chess(char chess, char x, int y) : chess(chess), x(x), y(y) {}
+
+    [[nodiscard]] string toString() const {
+        string output;
+        if (chess != 'P' && chess != 'p') {
+            output.push_back(toupper(chess));
+        }
+        output.push_back(x);
+        output.append(to_string(y));
+        return output;
+    }
+};
+
+bool cmpFunc(const Chess &lhs, const Chess &rhs) {
+    int lhs_type = orderOfType[tolower(lhs.chess)];
+    int rhs_type = orderOfType[tolower(rhs.chess)];
+    if (lhs_type == rhs_type) {
+        // Policy for black chess
+        if (islower(lhs.chess)) {
+            if (lhs.y == rhs.y) {
+                return (lhs.x < rhs.x);
+            }
+            return (lhs.y > rhs.y);
+        }
+            // Policy for white chess
+        else {
+            if (lhs.y == rhs.y) {
+                return (lhs.x < rhs.x);
+            }
+            return (lhs.y < rhs.y);
+        }
+    }
+    return (lhs_type > rhs_type);
+}
+
+vector<Chess> White;
+vector<Chess> Black;
+vector<vector<char> > chessboard;
+
+inline static
+void parse(const string &s) {
+    vector<char> tempVec;
+    for (size_t i = 2; i < s.size(); i += 4) {
+        if (isalpha(s.at(i))) {
+            tempVec.emplace_back(s.at(i));
+        } else {
+            tempVec.emplace_back('\0');
+        }
+    }
+    chessboard.emplace_back(tempVec);
+}
+
+inline static
+pair<char, int> convert(int row, int col) {
+    pair<char, int> result;
+    result.first = (char) col + 'a';
+    result.second = 8 - row;
+    return result;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    string s;
+    for (int i = 0; i < 17; ++i) {
+        getline(cin, s);
+        if (i % 2 != 0) {
+            parse(s);
+        }
+    }
+
+    int row = static_cast<int>(chessboard.size());
+    int col = static_cast<int>(chessboard.at(0).size());
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            if (isupper(chessboard.at(i).at(j))) {
+                auto p = convert(i, j);
+                White.emplace_back(Chess(chessboard.at(i).at(j), p.first, p.second));
+            } else if (islower(chessboard.at(i).at(j))) {
+                auto p = convert(i, j);
+                Black.emplace_back(Chess(chessboard.at(i).at(j), p.first, p.second));
+            }
+        }
+    }
+
+    sort(White.begin(), White.end(), cmpFunc);
+    sort(Black.begin(), Black.end(), cmpFunc);
+
+    // output
+    bool firstItem = true;
+    cout << "White: ";
+    for (const auto &i : White) {
+        if (firstItem) {
+            firstItem = false;
+        } else {
+            cout << ",";
+        }
+        cout << i.toString();
+    }
+    cout << endl;
+
+    firstItem = true;
+    cout << "Black: ";
+    for (const auto &i : Black) {
+        if (firstItem) {
+            firstItem = false;
+        } else {
+            cout << ",";
+        }
+        cout << i.toString();
+    }
+    cout << endl;
+    return 0;
+}
