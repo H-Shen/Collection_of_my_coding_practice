1a292a9eea667878bc60e3de7ca08a78ea2bc5d0
harrison <haohu.shen@ucalgary.ca>
2019-10-11 11:12:37 -0600

    updated

--- Miscellaneous/stringSplit.cpp
+++ Miscellaneous/stringSplit.cpp
@@ -1,16 +1,9 @@
-/**
- * Python3 is used to interactively help code testing.
- */
-#include <Python.h>
 #include <bits/stdc++.h>
-
-#define DEBUG
 
 /**
  * Split a string by using single character as the delimiter.
  * @return a vector of strings after splitting.
  */
-
 inline static
 std::vector<std::string> splitByChar(std::string s, const char &delim) {
     std::vector<std::string> res;
@@ -46,15 +39,25 @@ std::vector<std::string> splitByString(std::string s, const std::string &delim) 
     return result;
 }
 
-#ifdef DEBUG
+/**
+ * Split a string by whitespaces with unknown length.
+ */
+inline static
+std::vector<std::string> splitByWhitespaces(const std::string &s) {
+    std::istringstream iss(s);
+    return std::vector<std::string>{std::istream_iterator<std::string>(iss),
+                                    std::istream_iterator<std::string>()};
+}
 
 constexpr static size_t MIN_STRING_LEN = 50;
 constexpr static size_t MAX_STRING_LEN = 100;
-const static std::vector<char> charSet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-                                     'g', 'h',
-                                     'i',
-                                     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                                     'z'};
+const static std::vector<char> charSet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+                                          'f',
+                                          'g', 'h',
+                                          'i',
+                                          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                                          'y',
+                                          'z'};
 static std::random_device dev;
 static std::mt19937 random_generator(dev());
 static std::uniform_int_distribution<size_t> range(0, charSet.size() - 1);
@@ -76,42 +79,42 @@ std::string generateRandomString() {
 }
 
 inline static
+std::string generateRandomString(size_t strlength) {
+    std::string s;
+    for (size_t i = 0; i != strlength; ++i) {
+        s.push_back(getRandomAlnum());
+    }
+    return s;
+}
+
+inline static
 std::string toString(const std::vector<std::string> &A) {
-    std::string res = "[";
+    std::string res;
     bool firstItem = true;
     for (const std::string &i : A) {
         if (firstItem) {
             firstItem = false;
         } else {
-            res.append(", ");
+            res.append(" ");
         }
-        res.push_back('\'');
         res.append(i);
-        res.push_back('\'');
     }
-
-    res.push_back(']');
     return res;
 }
 
-inline static
-void f(const std::string &s, const char &delimiter) {
-    Py_Initialize();
-    // TODO
-
-
-    Py_Finalize();
-}
-
-#endif
-
 int main() {
 
-#ifdef DEBUG
-
-    // TODO
-
-#endif
+    // splitByWhitespaces
+    int testNumber = 10;
+    for (int i = 0; i < testNumber; ++i) {
+        std::string tempString;
+        for (int gap = 0; gap < testNumber; ++gap) {
+            tempString.append(generateRandomString(5));
+            tempString.append("\t   ");
+        }
+        std::cout << tempString << std::endl;
+        std::cout << toString(splitByWhitespaces(tempString)) << std::endl;
+    }
 
     return 0;
 }
