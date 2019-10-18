23c603b79420811241daeb4a448a3c229e0170a6
harrison <haohu.shen@ucalgary.ca>
2019-10-18 00:19:05 -0600

    updated

---
+++ Kattis/crne.cpp
@@ -0,0 +1,22 @@
+// https://open.kattis.com/problems/crne
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+
+    // input
+    ull N;
+    scanf("%llu", &N);
+    if (N % 2 == 0) {
+        ull a = N / 2;
+        printf("%llu\n", (1 + a) * (1 + a));
+    } else {
+        ull a = (N - 1) / 2;
+        printf("%llu\n", (1 + a) * (2 + a));
+    }
+
+    return 0;
+}
---
+++ Kattis/simplicity.cpp
@@ -0,0 +1,35 @@
+// https://open.kattis.com/problems/simplicity
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int MAXN = 105;
+char s[MAXN];
+vector<pair<char, int> > L;
+unordered_map<char, int> A;
+
+int main() {
+
+    // input
+    scanf("%s", s);
+    for (int i = 0; s[i] != '\0'; ++i) {
+        ++A[s[i]];
+    }
+    for (const auto &i : A) {
+        L.emplace_back(make_pair(i.first, i.second));
+    }
+    sort(L.begin(), L.end(), [](const pair<char, int> &lhs, const pair<char, int> &rhs) {
+       return (lhs.second > rhs.second);
+    });
+    int counter = 0;
+    while (L.size() > 2) {
+        while (L.back().second != 0) {
+            --L.back().second;
+            ++counter;
+        }
+        L.pop_back();
+    }
+    printf("%d\n", counter);
+
+    return 0;
+}
