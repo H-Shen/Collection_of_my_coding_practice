4352874345e52193b45a91e5d0875d26b57177a8
harrison <haohu.shen@ucalgary.ca>
2019-10-19 10:32:07 -0600

    updated

---
+++ Kattis/heartrate.cpp
@@ -0,0 +1,20 @@
+// https://open.kattis.com/problems/heartrate
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    double b, p, bpm, abpm;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%lf %lf", &b, &p);
+        bpm = 60 * b / p;
+        abpm = 60 * 1.0 / p;
+        printf("%.4lf %.4lf %.4lf\n", bpm - abpm, bpm, bpm + abpm);
+    }
+
+    return 0;
+}
