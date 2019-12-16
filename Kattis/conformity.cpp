feacbfcb349d112881f663ff26f60eff5e674785
harrison <haohu.shen@ucalgary.ca>
2019-12-16 09:54:28 -0700

    updated

---
+++ Kattis/conformity.cpp
@@ -0,0 +1,42 @@
+// https://open.kattis.com/problems/conformity
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int MAXN = 5;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    unordered_map<string, int> freq;
+    vector<int> A(MAXN);
+    while (n--) {
+        for (auto &i : A) {
+            cin >> i;
+        }
+        sort(A.begin(), A.end());
+        string s;
+        for (const auto &i : A) {
+            s.append(to_string(i));
+            s.push_back(' ');
+        }
+        ++freq[s];
+    }
+
+    int maxVal = std::max_element(freq.begin(), freq.end(),
+            [](const pair<string, int>& p1, const pair<string, int>& p2) {
+                return p1.second < p2.second; }
+        )->second;
+
+    int counter = 0;
+    for (const auto &i : freq) {
+        if (i.second == maxVal) {
+            counter += maxVal;
+        }
+    }
+    cout << counter << endl;
+
+    return 0;
+}
