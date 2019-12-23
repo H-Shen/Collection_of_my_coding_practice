2adfd0c7358f0466df4b773560f28e47e52fd6eb
Haohu Shen <hshen@Haohus-MacBook-Pro.local>
2019-12-22 20:57:58 -0700

    updated

---
+++ Kattis/kafkaesque.cpp
@@ -0,0 +1,27 @@
+// https://open.kattis.com/problems/kafkaesque
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int k;
+    int counter = 0;
+    cin >> k;
+    deque<int> L(k);
+    for (auto &i : L) {
+        cin >> i;
+    }
+    while (!L.empty()) {
+        for (int i = 1; i <= 100; ++i) {
+            if (i == L.front()) {
+                L.pop_front();
+            }
+        }
+        ++counter;
+    }
+    cout << counter << endl;
+
+    return 0;
+}
