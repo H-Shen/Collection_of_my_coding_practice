96e20ed96f2945243146ff0b284ef20f98edad01
Haohu Shen <hshen@pfo6wPODfC3StcrqJ.local>
2019-12-26 12:33:08 -0700

    updated

---
+++ Kattis/friday.cpp
@@ -0,0 +1,35 @@
+// https://open.kattis.com/problems/friday
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int day;
+
+int main() {
+
+    int t, m, d, counter;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d %d", &d, &m);
+        vector<int> A(static_cast<size_t>(m));
+        for (auto &i : A) {
+            scanf("%d", &i);
+        }
+        counter = 0;
+        day = 7;
+        for (const auto &i : A) {
+            for (int j = 1; j <= i; ++j) {
+                if (day == 5 && j == 13) {
+                    ++counter;
+                }
+                ++day;
+                if (day == 8) {
+                    day = 1;
+                }
+            }
+        }
+        printf("%d\n", counter);
+    }
+    return 0;
+}
---
+++ Kattis/queens.cpp
@@ -0,0 +1,50 @@
+// https://open.kattis.com/problems/queens
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> col;
+vector<int> row;
+unordered_set<int> mainDiagonal;
+unordered_set<int> subDiagonal;
+
+int main() {
+
+    int t, x, y, temp;
+    scanf("%d", &t);
+    col.resize(t);
+    row.resize(t);
+
+    for (int i = 0; i < t; ++i) {
+        scanf("%d %d", &x, &y);
+        if (col.at(x) == 1) {
+            printf("INCORRECT\n");
+            return 0;
+        } else {
+            col.at(x) = 1;
+        }
+        if (row.at(y) == 1) {
+            printf("INCORRECT\n");
+            return 0;
+        } else {
+            row.at(y) = 1;
+        }
+        temp = y - x;
+        if (mainDiagonal.find(temp) != mainDiagonal.end()) {
+            printf("INCORRECT\n");
+            return 0;
+        } else {
+            mainDiagonal.insert(temp);
+        }
+        temp = x + y;
+        if (subDiagonal.find(temp) != subDiagonal.end()) {
+            printf("INCORRECT\n");
+            return 0;
+        } else {
+            subDiagonal.insert(temp);
+        }
+    }
+    printf("CORRECT\n");
+    return 0;
+}
