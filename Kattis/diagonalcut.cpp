77fd7630bef763dba2010bfadf887bcdb526c074
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-02 01:35:36 -0700

    updated

---
+++ Kattis/diagonalcut.cpp
@@ -0,0 +1,28 @@
+// https://open.kattis.com/problems/diagonalcut
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    ll a, b;
+    cin >> a >> b;
+    ll c = __gcd(a, b);
+
+    a /= c;
+    b /= c;
+    if (a % 2 == 0) {
+        cout << 0 << endl;
+    }
+    else if (b % 2 == 0) {
+        cout << 0 << endl;
+    }
+    else {
+        cout << c << endl;
+    }
+
+    return 0;
+}
