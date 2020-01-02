b9d8843f8776a5b75245fc970f1bb60b8883c1d0
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-01 23:49:42 -0700

    updated

---
+++ Kattis/integerdivision.cpp
@@ -0,0 +1,26 @@
+// https://open.kattis.com/problems/integerdivision
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+unordered_map<int, ll> A;
+
+int main() {
+
+    int n;
+    int d;
+    int val;
+    scanf("%d %d", &n, &d);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &val);
+        ++A[val / d];
+    }
+    ll sum = 0;
+    for (const auto [k, v] : A) {
+        sum += v * (v - 1) / 2;
+    }
+    printf("%lld\n", sum);
+
+    return 0;
+}
---
+++ Kattis/shiritori.cpp
@@ -0,0 +1,47 @@
+// https://open.kattis.com/problems/shiritori
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    int n;
+    unordered_set<string> A;
+    string s;
+    cin >> n;
+    bool playerOne = true;
+    string lastWord;
+    while (n--) {
+        cin >> s;
+        if (A.find(s) != A.end()) {
+            if (playerOne) {
+                cout << "Player 1 lost" << endl;
+            } else {
+                cout << "Player 2 lost" << endl;
+            }
+            return 0;
+        }
+        if (lastWord.empty()) {
+            A.insert(s);
+            lastWord = s;
+        } else {
+            if (s.front() != lastWord.back()) {
+                if (playerOne) {
+                    cout << "Player 1 lost" << endl;
+                } else {
+                    cout << "Player 2 lost" << endl;
+                }
+                return 0;
+            } else {
+                A.insert(s);
+                lastWord = s;
+            }
+        }
+        playerOne = !playerOne;
+    }
+    cout << "Fair Game" << endl;
+
+    return 0;
+}
