9652361677896675bee40356de4137090bb16085
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-04 15:52:22 -0700

    updated

---
+++ Kattis/chartingprogress.cpp
@@ -0,0 +1,93 @@
+// https://open.kattis.com/problems/chartingprogress
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+inline static
+vector<vector<char> > rotate(const vector<vector<char> > &A) {
+    int row = static_cast<int>(A.size());
+    int col = static_cast<int>(A.front().size());
+    vector<vector<char> > temp(col, vector<char>(row));
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            temp.at(j).at(i) = A.at(i).at(j);
+        }
+    }
+    return temp;
+}
+
+inline static
+vector<vector<char> > process(const vector<vector<char> > &A) {
+    int row = static_cast<int>(A.size());
+    int col = static_cast<int>(A.front().size());
+    auto temp = rotate(A);
+    vector<int> ret;
+    for (int i = 0; i < col; ++i) {
+        for (int j = 0; j < row; ++j) {
+            if (temp.at(i).at(j) == '*') {
+                ret.emplace_back(j);
+            }
+        }
+    }
+    sort(ret.begin(), ret.end(), greater<>());
+
+    // reset all elements to '.'
+    for (int i = 0; i < col; ++i) {
+        for (int j = 0; j < row; ++j) {
+            temp.at(i).at(j) = '.';
+        }
+    }
+
+    auto iter = ret.cbegin();
+    for (int i = 0; i < col; ++i) {
+        temp.at(i).at(*iter) = '*';
+        ++iter;
+    }
+
+    return rotate(temp);
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    string s;
+    vector<vector<char> > L;
+    bool firstItem = true;
+    while (getline(cin, s)) {
+        if (!s.empty()) {
+            vector<char> temp;
+            for (const auto &i : s) {
+                temp.emplace_back(i);
+            }
+            L.emplace_back(temp);
+        } else {
+            if (firstItem) {
+                firstItem = false;
+            } else {
+                cout << endl;
+            }
+            L = process(L);
+            for (const auto &i : L) {
+                for (const auto &j : i) {
+                    cout << j;
+                }
+                cout << endl;
+            }
+            vector<vector<char> >().swap(L);
+        }
+    }
+    if (!firstItem) {
+        cout << endl;
+    }
+    L = process(L);
+    for (const auto &i : L) {
+        for (const auto &j : i) {
+            cout << j;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
---
+++ Kattis/commercials.cpp
@@ -0,0 +1,43 @@
+// https://open.kattis.com/problems/commercials
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll maxSubsequence(const vector<ll> &A) {
+
+    ll n = static_cast<int>(A.size());
+    ll currentSum = 0, maxSum = 0, interval = A[0];
+    for (int i = 0; i < n; i++) {
+        currentSum += A[i];
+
+        if (A[i] <= 0 && A[i] > interval) {
+            interval = A[i];
+        }
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+        else if (currentSum < 0)
+            currentSum = 0;
+    }
+
+    if (maxSum == 0)
+        return interval;
+    return maxSum;
+}
+
+int main() {
+
+    int n;
+    ll p;
+    scanf("%d %lld", &n, &p);
+    vector<ll> A(n);
+    for (auto &i : A) {
+        scanf("%lld", &i);
+        i -= p;
+    }
+    printf("%lld\n", maxSubsequence(A));
+
+    return 0;
+}
