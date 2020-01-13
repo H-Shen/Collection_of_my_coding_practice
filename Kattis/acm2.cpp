218da91f12cbab0cc3b1061bc67eb3b89d357b59
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-13 01:22:14 -0700

    updated

---
+++ Kattis/acm2.cpp
@@ -0,0 +1,44 @@
+// https://open.kattis.com/problems/acm2
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int MAX_TIME = 300;
+
+int main() {
+
+    int n, p, val;
+    scanf("%d %d", &n, &p);
+    vector<int> A;
+    int num_ac = 0;
+    int penalty_time = 0;
+    int counter = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &val);
+        if (i == p) {
+            counter += val;
+            if (counter > MAX_TIME) {
+                break;
+            } else {
+                ++num_ac;
+                penalty_time += counter;
+            }
+        } else {
+            A.emplace_back(val);
+        }
+    }
+    sort(A.begin(), A.end());
+
+    for (const auto &i : A) {
+        counter += i;
+        if (counter > MAX_TIME) {
+            break;
+        } else {
+            ++num_ac;
+            penalty_time += counter;
+        }
+    }
+    printf("%d %d\n", num_ac, penalty_time);
+
+    return 0;
+}
---
+++ Kattis/register.cpp
@@ -0,0 +1,27 @@
+// https://open.kattis.com/problems/register
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+constexpr int MAXN = 8;
+constexpr ll UPPER = 9699689;
+
+int main() {
+
+    vector<ll> A(MAXN);
+    for (auto &i : A) {
+        scanf("%lld", &i);
+    }
+    vector<ll> B = {1,2,6,30,210,2310,30030,510510};
+    ll sum = 0;
+    for (int i = 0; i < MAXN; ++i) {
+        sum += A.at(i) * B.at(i);
+        if (sum > UPPER) {
+            printf("0\n");
+            return 0;
+        }
+    }
+    printf("%lld\n", UPPER - sum);
+    return 0;
+}
