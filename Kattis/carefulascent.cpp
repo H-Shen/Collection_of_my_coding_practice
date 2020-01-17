9d2ef37325d085f2993f6cd6d7b247fd331ff981
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-16 23:57:01 -0700

    updated

---
+++ Kattis/carefulascent.cpp
@@ -0,0 +1,39 @@
+// https://open.kattis.com/problems/carefulascent
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int lower;
+    int upper;
+    double factor;
+
+    Node() = delete;
+
+    Node(int lower, int upper, double factor) : lower(lower), upper(upper),
+                                                factor(factor) {}
+};
+
+int main() {
+
+    int x, y, n, lower, upper;
+    double factor;
+    scanf("%d %d", &x, &y);
+    scanf("%d", &n);
+    vector<Node> A;
+    int diff = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d %d %lf", &lower, &upper, &factor);
+        A.emplace_back(Node(lower, upper, factor));
+    }
+    double sum = 0.0;
+    for (const auto &[l, u, f] : A) {
+        sum += (u - l) * f;
+        diff += u - l;
+    }
+    sum += y - diff;
+    printf("%.8lf\n", x * 1.0 / sum);
+
+    return 0;
+}
---
+++ Others/Luogu/P1601.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace BigInteger {
+
+    static const int LEN = 1005;
+    int a[LEN], b[LEN], c[LEN];
+
+    void clear(int a[LEN]) {
+        for (int i = 0; i < LEN; ++i) {
+            a[i] = 0;
+        }
+    }
+
+    void read(int a[LEN]) {
+        static char s[LEN + 1];
+        scanf("%s", s);
+        clear(a);
+        int len = strlen(s);
+        for (int i = 0; i < len; ++i) {
+            a[len - i - 1] = s[i] - '0';
+        }
+    }
+
+    void print(int a[LEN]) {
+        int i;
+        for (i = LEN - 1; i >= 1; --i)
+            if (a[i] != 0) {
+                break;
+            }
+        for (; i >= 0; --i) {
+            putchar(a[i] + '0');
+        }
+        putchar('\n');
+    }
+
+    void add(int a[LEN], int b[LEN], int c[LEN]) {
+        clear(c);
+        for (int i = 0; i < LEN - 1; ++i) {
+            c[i] += a[i] + b[i];
+            if (c[i] >= 10) {
+                c[i + 1] += 1;
+                c[i] -= 10;
+            }
+        }
+    }
+}
+
+int main() {
+
+    BigInteger::read(BigInteger::a);
+    BigInteger::read(BigInteger::b);
+    BigInteger::add(BigInteger::a, BigInteger::b, BigInteger::c);
+    BigInteger::print(BigInteger::c);
+
+    return 0;
+}
