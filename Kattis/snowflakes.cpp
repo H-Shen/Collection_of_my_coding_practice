bc7513d0161f7a13ee1c9ec731ce8fa8ca5627fe
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-19 22:17:50 -0700

    updated

---
+++ Kattis/snowflakes.cpp
@@ -0,0 +1,48 @@
+// https://open.kattis.com/problems/snowflakes
+// Longest subarray with unique elements
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(const vector<int> &A) {
+    int ret = 0;
+    int tail = 0;
+    int head = 0;
+    unordered_set<int> temp_set;
+    int n = static_cast<int>(A.size());
+    while (head < n) {
+        if (temp_set.find(A.at(head)) == temp_set.end()) {
+            temp_set.insert(A.at(head));
+            ret = max(ret, head - tail + 1);
+            ++head;
+        } else {
+            while (temp_set.find(A.at(head)) != temp_set.end() && tail != head) {
+                temp_set.erase(A.at(tail));
+                ++tail;
+            }
+            if (tail == head) {
+                temp_set.insert(A.at(head));
+                ret = max(ret, head - tail + 1);
+                ++head;
+            }
+        }
+    }
+    return ret;
+}
+
+int main() {
+
+    int t;
+    int n;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        vector<int> A(n);
+        for (auto &i : A) {
+            scanf("%d", &i);
+        }
+        printf("%d\n", solve(A));
+    }
+    
+    return 0;
+}
