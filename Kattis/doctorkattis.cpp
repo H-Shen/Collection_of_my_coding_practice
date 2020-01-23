e6cec7a10478e755e66133db15f30c430d07a5ff
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-23 01:08:55 -0700

    updated

---
+++ Kattis/doctorkattis.cpp
@@ -0,0 +1,60 @@
+// https://open.kattis.com/problems/doctorkattis
+//
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+constexpr int MAXN = 20;
+char name[MAXN];
+
+struct Cat {
+    string name;
+    int infection_level;
+    int index;
+    bool operator < (const Cat &other) const {
+        if (infection_level == other.infection_level) {
+            return (index < other.index);
+        }
+        return (infection_level > other.infection_level);
+    }
+    explicit Cat(string name, int infection_level, int index) : name(std::move(name)), infection_level(infection_level), index(index) {}
+    Cat() = default;
+};
+
+unordered_map<string, Cat> name2obj;
+tree<Cat, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> RBTree;
+
+int main() {
+
+    int n, op, val;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &op);
+        if (op == 0) {
+            scanf("%s %d", name, &val);
+            Cat temp_cat(name, val, i);
+            RBTree.insert(temp_cat);
+            name2obj[name] = temp_cat;
+        } else if (op == 1) {
+            scanf("%s %d", name, &val);
+            RBTree.erase(name2obj[name]);
+            name2obj[name].infection_level += val;
+            RBTree.insert(name2obj[name]);
+        } else if (op == 2) {
+            scanf("%s", name);
+            RBTree.erase(name2obj[name]);
+            name2obj.erase(name);
+        } else {
+            if (RBTree.empty()) {
+                printf("The clinic is empty\n");
+            } else {
+                printf("%s\n", RBTree.find_by_order(0)->name.c_str());
+            }
+        }
+    }
+
+
+    return 0;
+}
---
+++ Kattis/teque.cpp
@@ -0,0 +1,91 @@
+// https://open.kattis.com/problems/teque
+//
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+constexpr int MAXN = 20;
+char op[MAXN];
+
+struct Teque {
+    deque<int> A;
+    deque<int> B;
+    int get(int i) {
+        int length_A = static_cast<int>(A.size());
+        if (i < length_A) {
+            return A.at(i);
+        }
+        return B.at(i - length_A);
+    }
+    void push_front(int x) {
+        A.push_front(x);
+        balance();
+    }
+    void push_back(int x) {
+        B.push_back(x);
+        balance();
+    }
+    void push_middle(int x) {
+        B.push_front(x);
+        balance();
+    }
+    void balance() {
+        int length_A = static_cast<int>(A.size());
+        int length_B = static_cast<int>(B.size());
+        if ((length_A + length_B) % 2 == 0) {
+            // a | a
+            while (length_A != length_B) {
+                if (length_A > length_B) {
+                    B.push_front(A.back());
+                    A.pop_back();
+                    ++length_B;
+                    --length_A;
+                }
+                else {
+                    A.push_back(B.front());
+                    B.pop_front();
+                    ++length_A;
+                    --length_B;
+                }
+            }
+        } else {
+            // a | a-1
+            while (length_A != length_B + 1) {
+                if (length_A > length_B + 1) {
+                    B.push_front(A.back());
+                    A.pop_back();
+                    ++length_B;
+                    --length_A;
+                } else {
+                    A.push_back(B.front());
+                    B.pop_front();
+                    ++length_A;
+                    --length_B;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+
+    int n, val;
+    Teque T;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%s %d", op, &val);
+        if (strcmp(op, "push_back") == 0) {
+            T.push_back(val);
+        } else if (strcmp(op, "push_front") == 0) {
+            T.push_front(val);
+        } else if (strcmp(op, "push_middle") == 0) {
+            T.push_middle(val);
+        } else {
+            printf("%d\n", T.get(val));
+        }
+    }
+
+
+    return 0;
+}
