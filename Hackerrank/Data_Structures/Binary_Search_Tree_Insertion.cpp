6b5b58bccc03ae8fb89d1cf73eb95f74d91e245a
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-26 00:33:13 -0700

    updated

---
+++ Hackerrank/Data_Structures/Binary_Search_Tree_Insertion.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstddef>
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  
+    void preOrder(Node *root) {
+
+          if( root == NULL )
+              return;
+
+          std::cout << root->data << " ";
+
+          preOrder(root->left);
+          preOrder(root->right);
+      }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+
+    Node * insert(Node * root, int data) {
+        if (root == nullptr) {
+            root = new Node(data);
+        }
+        else if (data < root->data) {
+            if (root->left == nullptr) {
+                root->left = new Node(data);
+            } else {
+                root->left = insert(root->left, data);
+            }
+        }
+        else if (data > root->data) {
+            if (root->right == nullptr) {
+                root->right = new Node(data);
+            } else {
+                root->right = insert(root->right, data);
+            }
+        } else {
+            // Duplicated values will be ignored.
+        }
+
+        return root;
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+	myTree.preOrder(root);
+    return 0;
+}
