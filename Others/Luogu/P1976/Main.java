d09e196d578fa8f807482c0eaac4624aae05aa78
Haohu Shen <haohu.shen@ucalgary.ca>
2020-02-22 23:30:02 -0700

    updated

---
+++ Others/Luogu/P1976/Main.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger().add(BigInteger.ONE);
+        BigInteger C_i_1 = BigInteger.ONE;
+        BigInteger Four = new BigInteger("4");
+        BigInteger Two = new BigInteger("2");
+        BigInteger Mod = new BigInteger("100000007");
+        BigInteger C_i = C_i_1;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(n) < 0; i = i.add(BigInteger.ONE)) {
+            C_i = i.multiply(Four).subtract(Two).multiply(C_i_1).divide(i.add(BigInteger.ONE));
+            C_i_1 = C_i;
+        }
+        System.out.println(C_i.mod(Mod));
+    }
+}
