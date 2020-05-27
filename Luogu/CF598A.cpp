d43ae565a2ed9afd9f73ff941d7116e097b24321
Haohu Shen <haohu.shen@ucalgary.ca>
2020-05-26 22:33:03 -0600

    updated

---
+++ Luogu/CF598A.cpp
@@ -0,0 +1,92 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+
+namespace IO {
+    template<typename T>
+    inline
+    void read(T &t) {
+        int n = 0;
+        int c = getchar();
+        t = 0;
+        while (!isdigit(c)) n |= c == '-', c = getchar();
+        while (isdigit(c)) t = t * 10 + c - 48, c = getchar();
+        if (n) t = -t;
+    }
+
+    template<typename T, typename... Args>
+    inline
+    void read(T &t, Args &... args) {
+        read(t);
+        read(args...);
+    }
+
+    template<typename T>
+    inline void write(T x) {
+        if (x < 0) x = -x, putchar('-');
+        if (x > 9) write(x / 10);
+        putchar(x % 10 + 48);
+    }
+
+    template<typename T>
+    inline void writeln(T x) {
+        write(x);
+        putchar('\n');
+    }
+}
+
+vector<ll> power_of_two = {
+        1,
+        2,
+        4,
+        8,
+        16,
+        32,
+        64,
+        128,
+        256,
+        512,
+        1024,
+        2048,
+        4096,
+        8192,
+        16384,
+        32768,
+        65536,
+        131072,
+        262144,
+        524288,
+        1048576,
+        2097152,
+        4194304,
+        8388608,
+        16777216,
+        33554432,
+        67108864,
+        134217728,
+        268435456,
+        536870912,
+        1073741824,
+        2147483648,
+        4294967296,
+        8589934592
+};
+
+int main() {
+
+    int t;
+    ll n, sum;
+    IO::read(t);
+    while (t--) {
+        IO::read(n);
+        auto p_ = upper_bound(power_of_two.begin(), power_of_two.end(), n);
+        --p_;
+        auto p = p_ - power_of_two.begin();
+        sum = (1 + n) * n / 2 - 2 * (power_of_two.at(p + 1) - 1);
+        IO::writeln(sum);
+    }
+
+    return 0;
+}
