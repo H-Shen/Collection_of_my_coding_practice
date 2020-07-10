7483304dddb082e0e8b41c2625a5d6e2dd3ec4ed
Haohu Shen <haohu.shen@ucalgary.ca>
2020-07-10 00:58:04 -0600

    updated

--- Luogu/P2369.cpp
+++ Luogu/P2369.cpp
@@ -1,6 +1,34 @@
-#include <bits/stdc++.h>
+#include <bits/extc++.h>
 
 using namespace std;
+using ll = long long;
+
+namespace IO {
+    template <typename T>
+    inline
+    void read(T& t) {
+        int n = 0; int c = getchar_unlocked(); t = 0;
+        while (!isdigit(c)) n |= c == '-', c = getchar_unlocked();
+        while (isdigit(c)) t = t * 10 + c - 48, c = getchar_unlocked();
+        if (n) t = -t;
+    }
+    template <typename T, typename... Args>
+    inline
+    void read(T& t, Args&... args) {
+        read(t); read(args...);
+    }
+    template <typename T>
+    inline void write(T x) {
+        if (x < 0) x = -x, putchar_unlocked('-');
+        if (x > 9) write(x / 10);
+        putchar_unlocked(x % 10 + 48);
+    }
+    template <typename T>
+    inline void writeln(T x) {
+        write(x);
+        putchar_unlocked('\n');
+    }
+}
 
 priority_queue<int> pq;
 priority_queue<int, vector<int>, greater<> > pq_output;
@@ -8,12 +36,12 @@ int n, m, val;
 
 int main() {
 
-    scanf("%d %d", &n, &m);
+    IO::read(n, m);
     if (n == 0 || m == 0) {
         return 0;
     }
     while (n--) {
-        scanf("%d", &val);
+        IO::read(val);
         if (pq.size() < m) {
             pq.push(val);
         } else if (val < pq.top()) {
@@ -26,7 +54,7 @@ int main() {
         pq.pop();
     }
     while (!pq_output.empty()) {
-        printf("%d\n", pq_output.top());
+        IO::writeln(pq_output.top());
         pq_output.pop();
     }
     return 0;
