dba653983d1a80b5ae2ce8b86c7de132155d7f5b
Haohu Shen <haohu.shen@ucalgary.ca>
2020-08-10 23:34:24 -0600

    updated

---
+++ Luogu/CF952A.cpp
@@ -0,0 +1,12 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n % 2 << '\n';
+    return 0;
+}
---
+++ Luogu/CF962A.cpp
@@ -0,0 +1,95 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+
+#define CODEFORCES
+
+#ifdef CODEFORCES
+namespace IO {
+    template <typename T>
+    inline
+    void read(T& t) {
+        int n = 0; int c = _getchar_nolock(); t = 0;
+        while (!isdigit(c)) n |= c == '-', c = _getchar_nolock();
+        while (isdigit(c)) t = t * 10 + c - 48, c = _getchar_nolock();
+        if (n) t = -t;
+    }
+    template <typename T, typename... Args>
+    inline
+    void read(T& t, Args&... args) {
+        read(t); read(args...);
+    }
+    template <typename T>
+    inline void write(T x) {
+        if (x < 0) x = -x, _putchar_nolock('-');
+        if (x > 9) write(x / 10);
+        _putchar_nolock(x % 10 + 48);
+    }
+    template <typename T>
+    inline void writeln(T x) {
+        write(x);
+        _putchar_nolock('\n');
+    }
+}
+#else
+namespace IO {
+    template<typename T>
+    inline
+    void read(T &t) {
+        int n = 0;
+        int c = getchar_unlocked();
+        t = 0;
+        while (!isdigit(c)) n |= c == '-', c = getchar_unlocked();
+        while (isdigit(c)) t = t * 10 + c - 48, c = getchar_unlocked();
+        if (n) t = -t;
+    }
+
+    template<typename T, typename... Args>
+    inline
+    void read(T &t, Args &... args) {
+        read(t);
+        read(args...);
+    }
+
+    template<typename T>
+    inline void write(T x) {
+        if (x < 0) x = -x, putchar_unlocked('-');
+        if (x > 9) write(x / 10);
+        putchar_unlocked(x % 10 + 48);
+    }
+
+    template<typename T>
+    inline void writeln(T x) {
+        write(x);
+        putchar_unlocked('\n');
+    }
+}
+#endif
+
+int main() {
+
+    int n;
+    IO::read(n);
+    vector<ll> A(n);
+    for (auto &i : A) IO::read(i);
+    ll sum = accumulate(A.begin(), A.end(), 0LL);
+    ll half;
+    if (sum % 2 == 0) {
+        half = sum / 2;
+    } else {
+        half = sum / 2 + 1;
+    }
+    int counter = 0;
+    sum = 0;
+    for (const auto &i : A) {
+        if (sum >= half) {
+            break;
+        }
+        sum += i;
+        ++counter;
+    }
+    IO::writeln(counter);
+    return 0;
+}
