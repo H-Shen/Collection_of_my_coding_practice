7cc37b134024e48cbbc347a7583c8daa60ac8291
H-Shen <haohu.shen@ucalgary.ca>
2020-09-23 10:03:48 -0600

    update

---
+++ Hackerrank/Functional_Programming/Sum_of_Odd_Elements.hs
@@ -0,0 +1,7 @@
+-- https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem
+f arr = sum [x | x <- arr, mod x 2 /= 0]
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+	inputdata <- getContents
+	putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
---
+++ Hackerrank/Functional_Programming/Update_List.hs
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/fp-update-list/problem
+
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+
+f arr = [abs x | x <- arr]
+
+-- This section handles the Input/Output and can be used as it is. Do not modify it.
+main = do
+	inputdata <- getContents
+	mapM_ putStrLn $ map show $ f $ map (read :: String -> Int) $ lines inputdata
