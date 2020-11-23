2ee146e99275b1c49b1ab444adb128d3ff57f51c
H-Shen <haohu3991@gmail.com>
2020-11-22 21:52:21 -0700

    update

---
+++ Leetcode/103.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int h(TreeNode* root) {
+        int height = 0;
+        if (root) {
+            if (root->left && root->right) {
+                height = 1 + max(h(root->left), h(root->right));
+            } else if (root->left && !root->right) {
+                height = 1 + h(root->left);
+            } else if (!root->left && root->right) {
+                height = 1 + h(root->right);
+            }
+        }
+        return height;
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int> > result;
+        if (root) {
+            int currentLevel;
+            result.resize(h(root) + 1);
+            queue<pair<TreeNode*, int> > q;
+            q.push({root, 0});
+            while (!q.empty()) {
+                TreeNode* u = q.front().first;
+                currentLevel = q.front().second;
+                q.pop();
+                result.at(currentLevel).emplace_back(u->val);
+                if (u->left) {
+                    q.push({u->left, currentLevel + 1});
+                }
+                if (u->right) {
+                    q.push({u->right, currentLevel + 1});
+                }
+            }
+        }
+        for (size_t i = 0; i != result.size(); ++i) {
+            if (i % 2 != 0) {
+                reverse(result.at(i).begin(), result.at(i).end());
+            }
+        }
+        return result;
+    }
+};
---
+++ Leetcode/107.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int h(TreeNode* root) {
+        int height = 0;
+        if (root) {
+            if (root->left && root->right) {
+                height = 1 + max(h(root->left), h(root->right));
+            } else if (root->left && !root->right) {
+                height = 1 + h(root->left);
+            } else if (!root->left && root->right) {
+                height = 1 + h(root->right);
+            }
+        }
+        return height;
+    }
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int> > result;
+        if (root) {
+            int currentLevel;
+            result.resize(h(root) + 1);
+            queue<pair<TreeNode*, int> > q;
+            q.push({root, (int)result.size() - 1});
+            while (!q.empty()) {
+                TreeNode* u = q.front().first;
+                currentLevel = q.front().second;
+                q.pop();
+                result.at(currentLevel).emplace_back(u->val);
+                if (u->left) {
+                    q.push({u->left, currentLevel - 1});
+                }
+                if (u->right) {
+                    q.push({u->right, currentLevel - 1});
+                }
+            }
+        }
+        return result;
+    }
+};
