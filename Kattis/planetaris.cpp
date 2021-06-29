79d640290b332c91d260f6026479eefc8fbe9422
XTh3G4p <haohu.shen@ucalgary.ca>
2021-06-29 15:05:35 -0600

    Create planetaris.cpp

---
+++ Kattis/planetaris.cpp
@@ -0,0 +1,82 @@
+// https://open.kattis.com/problems/planetaris
+
+#include <bits/extc++.h>
+
+using namespace std;
+using ll = long long;
+
+namespace IO {
+    template<typename T>
+    inline
+    bool can_read(T &t) {
+        int n = 0;
+        int ch = getchar_unlocked();
+        while (!isdigit(ch)) {
+            if (ch == EOF) return false;
+            n |= ch == '-';
+            ch = getchar_unlocked();
+        }
+        t = 0;
+        while (isdigit(ch)) {
+            t = t * 10 + ch - 48;
+            ch = getchar_unlocked();
+        }
+        if (n) t = -t;
+        return true;
+    }
+
+    template<typename T, typename... Args>
+    inline
+    bool can_read(T &t, Args &... args) {
+        return can_read(t) && can_read(args...);
+    }
+
+    template<typename T>
+    inline
+    void read(T &t) {
+        int n = 0;
+        int c = getchar_unlocked();
+        t = 0;
+        while (!isdigit(c)) n |= c == '-', c = getchar_unlocked();
+        while (isdigit(c)) t = t * 10 + c - 48, c = getchar_unlocked();
+        if (n) t = -t;
+    }
+
+    template<typename T, typename... Args>
+    inline
+    void read(T &t, Args &... args) {
+        read(t);
+        read(args...);
+    }
+
+    template<typename T>
+    inline void write(T x) {
+        if (x < 0) x = -x, putchar_unlocked('-');
+        if (x > 9) write(x / 10);
+        putchar_unlocked(x % 10 + 48);
+    }
+
+    template<typename T>
+    inline void writeln(T x) {
+        write(x);
+        putchar_unlocked('\n');
+    }
+}
+
+int n, a;
+vector<int> A;
+
+int main() {
+    IO::read(n,a);
+    A.resize(n);
+    for (auto &i : A) IO::read(i);
+    sort(A.begin(), A.end());
+    int counter = 0;
+    for (const auto &i : A) {
+        if (a-(i+1)<0) break;
+        a -= (i+1);
+        ++counter;
+    }
+    IO::writeln(counter);
+    return 0;
+}
