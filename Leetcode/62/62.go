d19f6e9fb4fcb4b8de80055333449b9b8e8d119e
Haohu Shen <haohu3991@gmail.com>
2021-11-16 19:47:21 -0700

    update

---
+++ Leetcode/62/62.go
@@ -0,0 +1,21 @@
+var dp [][]int
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = 1
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1]
+	}
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
