2966de29e7b2f41959fe70779b09aa61e510a0ae
Haohu Shen <haohu3991@gmail.com>
2022-03-06 00:02:56 -0700

    update

--- Leetcode/42/42.cpp
+++ Leetcode/42/42.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     // O(nlogn)
-    // trap[i] = max(min(maxFromLeft[i-1], maxFromRight[i+1]) - height[i], 0)
     int trap(vector<int>& height) {
         int n = (int)height.size();
         if (n <= 2) return 0;
-        multiset<int, greater<int>> left, right;
-        left.insert(height[0]);
+        vector<int> maxFromLeft;
+        multiset<int, greater<int>> right;
+        maxFromLeft.emplace_back(height[0]);
         for (int i = 2; i < n; ++i) {
             right.insert(height[i]);
         }
-        multiset<int, greater<int>>::iterator iter1, iter2;
         int sum = 0;
         for (int i = 1; i < n-1; ++i) {
-            sum += max(min(*left.begin(), *right.begin()) - height[i], 0);
-            left.insert(height[i]);
+            sum += max(min(maxFromLeft.back(), *right.begin()) - height[i], 0);
+            maxFromLeft.emplace_back(max(maxFromLeft.back(), height[i]));
             right.erase(right.lower_bound(height[i+1]));
         }
         return sum;
